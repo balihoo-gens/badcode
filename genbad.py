3fe8675fe2b27e25513e8782580e17bc304b2c73
Gerard Ens <GerardEns@gmail.com>
2015-04-14 00:59:24 -0600

    worse code, better generation

--- genbad.py
+++ genbad.py
@@ -1,27 +1,60 @@
 from createdb import PlantDatabase
+import sys
 
 def gen_insert_val_clause(plant,system,sensor):
-    s="""        for row in c.execute("SELECT * FROM Plants"):
-            if row[1] == '%s':
-                plant = row[0]
-        for row in c.execute("SELECT * FROM Systems"):
-            if row[2] == plant and row[1] == "%s":
-                system = row[0]
-        for row in c.execute("SELECT * FROM Sensors"):
-            if row[2] == system and row[1] == "%s":
-                sensor = row[0]
+    s="""
+        sensor=0
+        for plant_row in c.execute("SELECT * FROM Plants WHERE Name = '%s' "):
+            for sys_row in c.execute("SELECT * FROM Systems WHERE Type = '%s'"):
+                for sens_row in c.execute("SELECT * FROM Sensors WHERE Type = '%s'"):
+                    if sys_row[2] == plant_row[0] and sens_row[2] == sys_row[0]:
+                        sensor = sens_row[0]
         value = factory.get_sensor_value('%s', '%s', '%s')
         c.execute("INSERT INTO SensorValues VALUES (" + str(time.time()) + "," + str(sensor) + "," + str(value) + ")")"""
     return s % (plant,system,sensor,plant,system,sensor)
 
-plants = ["PlantZero", "PlantOne", "PlantTwo", "PlantThree", "PlantFour"]
-for plant in plants:
-    for sys in PlantDatabase.system_types:
-        for sen in PlantDatabase.sensor_types:
-             print(gen_insert_val_clause(plant,sys,sen))
 
+def header():
+    return """
+###############################################
+## data loader. Run as a cron job evey minute
+###############################################
+import sqlite3
+import factory
+import time
 
-#now add some awful and complicated system status update code
+c = sqlite3.connect("plants.db").cursor()
 
-#then add some creative reports, like the average sensor values by location
+end = time.time() + 2
+last = time.time()
+while (time.time() < end):
+    if (time.time() - last > 1):
+        last = time.time()
+"""
+
+def sensor_insert_clauses():
+    badcode = ""
+    plants = ["PlantZero", "PlantOne", "PlantTwo", "PlantThree", "PlantFour"]
+    for plant in plants:
+        for sys in PlantDatabase.system_types:
+            for sen in PlantDatabase.sensor_types:
+                 badcode += gen_insert_val_clause(plant,sys,sen)
+    return badcode
+
+def generate(filename = None):
+    badcode = header() + sensor_insert_clauses()
+    #now add some awful and complicated system status update code
+    #then add some creative reports, like the average sensor values by location
+    if filename:
+        with open(filename, 'w') as f:
+            f.write(badcode)
+    else:
+        print(badcode)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        generate(sys.argv[1])
+    else:
+        generate()
+
 
